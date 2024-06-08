321aebfc96eb77033b3a74e8eeee17cf36bafaf2
Lucas <lucasgomesmorais58@gmail.com>
2024-06-07 22:02:42 -0300

    feat: add request

--- securepassword/src/main/java/com/example/securepassword/controller/PasswordRequest.java
+++ securepassword/src/main/java/com/example/securepassword/controller/PasswordRequest.java
@@ -1,2 +1,12 @@
-package com.example.securepassword.controller;public class PasswordRequest {
+package com.example.securepassword.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordRequest {
+    private String password;
 }
